59a39150bd18b49ee92d6f3ce207ff9d1a9c7312
Christian Hankel <christianahankel@gmail.com>
2020-01-28 07:14:59 -0600

    Style the header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,27 +1,35 @@
 import { createStyles, makeStyles } from '@material-ui/styles';
 import { Theme, Typography } from '@material-ui/core';
 import React from 'react';
+import { DonutLargeRounded } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme: Theme) => {
   return createStyles({
+    description: {
+      color: 'rgba(255, 255, 255, 0.7)',
+      fontSize: '1.25rem',
+      marginTop: '.75rem',
+    },
     logo: {
       marginRight: theme.spacing(),
     },
     header: {
-      padding: theme.spacing(3),
-      display: 'flex',
       alignItems: 'center',
-    },
+      display: 'flex',
+      flexDirection: 'column',
+      padding: theme.spacing(3),
+    }
   });
 });
 
 export interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = (props) => {
-  const classes = useStyles();
+  const classes = useStyles({});
   return (
     <header className={classes.header} data-test="Header">
-      <Typography variant="h5">Trivia!</Typography>
+      <Typography color='primary' variant='h3'>Trivia Empire</Typography>
+      <span className={classes.description}>Answer Questions. Vanquish Your Foes. Build Your Empire.</span>
     </header>
   );
 };
